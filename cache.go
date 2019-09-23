bf689ba4be2a53b03190b5fa391254f39a938b8b
bryntsev-ae <bryntsev-ae@fxclub.org>
2019-09-23 09:53:54 +0200

    oops

--- cache.go
+++ cache.go
@@ -1,7 +1,6 @@
 package cache
 
 import (
-	"sync"
 	"time"
 )
 
@@ -14,42 +13,4 @@ type Storage interface {
 type Item struct {
 	Content    []byte
 	Expiration int64
-}
-
-//Storage mecanism for caching strings in memory
-type Storage struct {
-	items map[string]Item
-	mu    *sync.RWMutex
-}
-
-//NewStorage creates a new in memory storage
-func NewStorage() *Storage {
-	return &Storage{
-		items: make(map[string]Item),
-		mu:    &sync.RWMutex{},
-	}
-}
-
-//Get a cached content by key
-func (s Storage) Get(key string) []byte {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	item := s.items[key]
-	if item.Expired() {
-		delete(s.items, key)
-		return nil
-	}
-	return item.Content
-}
-
-//Set a cached content by key
-func (s Storage) Set(key string, content []byte, duration time.Duration) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.items[key] = Item{
-		Content:    content,
-		Expiration: time.Now().Add(duration).UnixNano(),
-	}
 }
